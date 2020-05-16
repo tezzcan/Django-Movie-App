bb1053ed47b8dab9d870afe9476677d3300d6f18
Oğuzhan Tezcan <aslanozi55@gmail.com>
2020-05-16 16:32:24 +0300

    Initial commit
    Things I've done so far. Will be continue from here.

---
+++ django-venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tezcan\desktop\django_project\django-venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ firstDJ_project/movies/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+    isPublished = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_image_path(self):
+        return '/img/' + self.image
